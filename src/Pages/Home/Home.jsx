78f7127198b7e04366654150a39d8ba51d0b6055
Lift-Dev <liftupup0@gmail.com>
2025-04-11 23:37:48 +0300

    Navbar added

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import './Home.css'
+import Footer from '../../components/Footer/Footer'
+import Navbar from '../../components/Navbar/Navbar'
+
+function Home() {
+  return (
+    <div className='Home'>
+       <Navbar/>
+    </div>
+  )
+}
+
+export default Home
