1aa29b5e6874064f1d6bb07d38ad2b7ae3b14998
Lift-Dev <liftupup0@gmail.com>
2025-04-14 03:37:05 +0300

    Api added from Tmdb

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Home from './Pages/Home/Home'
 import { Routes, Route } from 'react-router-dom'
 import Login from './Pages/Login/Login'
+import Player from './Pages/Player/Player'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login/>} />
+        <Route path='/player/:id' element={<Player/>} />
       </Routes>
       
     </div>
--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,14 +1,26 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import card_data from '../../assets/cards/Cards_data'
-
-
+import { Link } from 'react-router-dom';
 
 
 
 const TitleCards = ({title, category}) => {
 
+  const [apiData, setApiData] = useState([]);
+
   const cardsRef = useRef();
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YWY3NDU4YTBmNDhkMjVmMDUxMDY2NWZhMTQyYTg4ZiIsIm5iZiI6MTc0NDU3OTA4NS43MzYsInN1YiI6IjY3ZmMyYTBkMzExMGJkODJkZmFkMGUxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f17OZTOqBSnYz5CsMB7qAz9cBGtlIUu8HxC-4AJ7-cE",
+    },
+  };
+
+ 
 
   const handlewheel = (event) => {
     event.preventDefault();
@@ -16,6 +28,13 @@ const TitleCards = ({title, category}) => {
   };
 
   useEffect(() => {
+
+     fetch(`https://api.themoviedb.org/3/movie/${category?
+        category:"now_playing"}?language=en-US&page=1`,options)
+       .then((res) => res.json())
+       .then((res) => setApiData(res.results))
+       .catch((err) => console.error(err));
+
     cardsRef.current.addEventListener("wheel", handlewheel);
   }, []);
   
@@ -23,12 +42,12 @@ const TitleCards = ({title, category}) => {
     <div className="title-cards">
           <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {card_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
-            <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
-            </div>
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+              <p>{card.original_title}</p>
+            </Link>
           );
         })}
       </div>
